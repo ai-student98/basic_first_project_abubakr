85077c9e29c0e2df7e6e873b346ba57e4af3d973
ai-student98 <alavimeh493@gmail.com>
2025-01-04 19:21:19 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,4 @@ with st.expander('Data'):
 with st.sidebar:
   st.header("Введите признаки: ")
   island = st.selectbox('Island', ('Torgersen', 'Dream', 'Biscoe'))
+  bill_lenght_mm = st.slider('Bill length (mm)', 32.1, 59.6, 44.5)
