e8591567c060e300c939cf1098aee06558849d79
ai-student98 <alavimeh493@gmail.com>
2025-01-04 19:05:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('😁😂 My first website')
 
 st.write('Тут я задеплою модель классификации')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander('Data'):
+  st.write("X")
